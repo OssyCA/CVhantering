8bf07e115669db90839092916178fc7f3b04dfab
Ossman <drake@OSSMAN>
2025-03-09 17:33:06 +0100

    Test

--- Dtos/PersonDto.cs
+++ Dtos/PersonDto.cs
@@ -2,6 +2,7 @@
 {
     public class PersonDto
     {
+
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public List<EducationDto>  Educations { get; set; }
